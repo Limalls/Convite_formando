659f2a698e44861279ddc2dd495dd8dc9ee26fe7
limalls <teodorolimadutra107@gmail.com>
2026-09-23 15:18:32 -0300

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/convite-formatura-roberta-react/',
+  base: '/Convite_formando/',
 
 
 
